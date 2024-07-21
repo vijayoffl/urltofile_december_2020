41069faad6c242f360d2b70f2e7240e1a565d5b1
vijayoffl <56537792+vijayoffl@users.noreply.github.com>
2024-07-21 13:14:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+class Config(object):
+    # get a token from https://chatbase.com
+    #CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1111076324:AAHYYFEy5whWQ3ufPJO8PaGyg-c2kdPsyZc")
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 1211432))
+    API_HASH = os.environ.get("API_HASH","736f868bb1190ba92b5571e62f5a202e")
+    # Get these values from my.telegram.org
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "829623994").split())
+    # Banned Unwanted Members..
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", 1900000000))
+    SP_LIT_ALGO_RITH_M = os.environ.get(
+        "SP_LIT_ALGO_RITH_M",
+        "rar"
+    )
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "122.175.19.164:80")
+    # https://t.me/hevcbay/951
+    OUO_IO_API_KEY = ""
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    # watermark file
+    DEF_WATER_MARK_FILE = ""
